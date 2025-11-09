5190de93c3cb4d0f6687f2434364d1b18c46719e
Francesco Brunelli <francescobrunelli@AirdiFrancesco4.homenet.telecomitalia.it>
2025-11-09 16:21:56 +0100

    updated readme for scripts

--- scripts/README.md
+++ scripts/README.md
@@ -1,1 +1,31 @@
 # Run/Plot Scripts
+
+Matrices:
+
+	### SMALL ###
+- fd12: 7.500x7.500 28.462nnz (129KB)
+wget https://suitesparse-collection-website.herokuapp.com/MM/Hohn/fd12.tar.gz
+
+- sinc12: 7.500x7.500 283.992nnz (1,5MB)
+wget https://suitesparse-collection-website.herokuapp.com/MM/Hohn/sinc12.tar.gz
+
+	### MEDIUM ###	
+- epb1: 14.724x14.734 95.053nnz (935KB)
+wget https://suitesparse-collection-website.herokuapp.com/MM/Averous/epb1.tar.gz
+
+- c-46: 14.913x14.913 130.397nnz (541KB)
+wget https://suitesparse-collection-website.herokuapp.com/MM/Schenk_IBMNA/c-46.tar.gz
+
+- human_gene2: 14.340x14.340 18.068.388nnz (60MB)
+wget https://suitesparse-collection-website.herokuapp.com/MM/Belcastro/human_gene2.tar.gz
+
+	### LARGE ###
+
+- jan99jac100: 34.454x34.454 190.224nnz (1,6MB)
+wget https://suitesparse-collection-website.herokuapp.com/MM/Hollinger/jan99jac100.tar.gz
+
+- ship_001: 34.920x34.920 3.896.496nnz (22MB)
+wget https://suitesparse-collection-website.herokuapp.com/MM/DNVS/ship_001.tar.gz
+
+
+
---
+++ scripts/plot_script.py
@@ -0,0 +1,38 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+ram_sizes = ["32GB", "64GB", "128GB"]
+data = {}
+
+for ram in ram_sizes:
+    x_list, y_list = [], []
+    directory = f"../results/1138_bus_results/{ram}/runtime/"
+    for file in os.scandir(directory):
+        with open(os.path.join(directory, file.name)) as f:
+            threads = int(f.readline().strip())
+            df = pd.read_csv(f, comment="#", skiprows=1)
+            p90 = np.percentile(df.iloc[:,0].values, 90)
+            x_list.append(threads)
+            y_list.append(p90)
+            print(f"{ram} - File {file.name}: p90 = {p90}")
+    # Sort
+    x_list, y_list = zip(*sorted(zip(x_list, y_list)))
+    data[ram] = (x_list, y_list)
+
+# Plot
+threads_ticks = data["32GB"][0]  # x-values for 32GB, same for all
+plt.xscale("log")
+plt.xticks(threads_ticks, threads_ticks)
+
+plt.grid(True, which="both", axis='y', ls='--')
+
+colors = {"32GB":"blue", "64GB":"green", "128GB":"red"}
+for ram in ram_sizes:
+    plt.plot(*data[ram], marker='o', color=colors[ram], label=ram)
+
+plt.title("Execution Time vs Thread Count for different RAM Sizes")
+plt.xlabel("No. of Threads")
+plt.ylabel("Execution Time")
+plt.legend(title = "Memory")
+plt.show()
