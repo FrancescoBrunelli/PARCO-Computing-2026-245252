2dccf1518b8147bf7db5c23661ad8e246cb86dd4
Francesco Brunelli <francescobrunelli@MacBook-Air-di-Francesco-4.local>
2025-11-10 10:15:21 +0100

    Resolved some bugs

--- scripts/run_perf_parallel_jobs.sh
+++ scripts/run_perf_parallel_jobs.sh
@@ -3,6 +3,8 @@
 MAX_THREADS=64
 INPUT="$1"    # Input passato da terminale
 
+g++ -std=c++11 -O3 -march=native -ftree-vectorize src/main.cpp src/matrix.cpp -o src/main.out
+
 for NTHREADS in 1 2 4 8 16 32 64
 do
     echo "Submitting job for NTHREADS=$NTHREADS"
