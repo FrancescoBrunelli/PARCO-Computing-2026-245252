2084a7390b9b3d90972fca767a7656d28ce0660d
Francesco Brunelli <francescobrunelli@MacBook-Air-di-Francesco-4.local>
2025-11-10 10:19:28 +0100

    Resolved some bugs

--- scripts/run_perf_parallel_jobs.sh
+++ scripts/run_perf_parallel_jobs.sh
@@ -8,5 +8,5 @@ g++ -std=c++11 -O3 -march=native -ftree-vectorize src/main.cpp src/matrix.cpp -o
 for NTHREADS in 1 2 4 8 16 32 64
 do
     echo "Submitting job for NTHREADS=$NTHREADS"
-    qsub -v INPUT="$INPUT",NTHREADS=$NTHREADS run_perf.pbs
+    qsub -v INPUT="$INPUT",NTHREADS=$NTHREADS scripts/run_perf.pbs
 done
