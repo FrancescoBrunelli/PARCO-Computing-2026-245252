86cdfeb37cf71fec8fb8b83bd329588c6c2b0369
Francesco Brunelli <francescobrunelli@MacBook-Air-di-Francesco-4.local>
2025-11-10 09:57:30 +0100

    Added thread parallel executions

---
+++ scripts/run_perf_parallel_jobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MAX_THREADS=64
+INPUT="$1"    # Input passato da terminale
+
+for NTHREADS in 1 2 4 8 16 32 64
+do
+    echo "Submitting job for NTHREADS=$NTHREADS"
+    qsub -v INPUT="$INPUT",NTHREADS=$NTHREADS run_perf.pbs
+done
