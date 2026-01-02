d7f7287c925a39acfc1256e41cb67359bcb4b5f9
FrancescoBrunelli <brunellifrancesco21@gmail.com>
2026-01-02 11:26:25 +0100

    Implemented 2D Partitioning

--- D2/src/main.cpp
+++ D2/src/main.cpp
@@ -175,7 +175,7 @@ int MPI_1D_Partitioning(int argc, char** argv) {
 			tmp = last - first;		// Actual number of rows for the i-th process
 			MPI_Send(&tmp, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
 
-			n = count_if(aRow.begin() + current_index, aRow.end(),  [first, last] (int val) {
+			n = count_if(aRow.begin() + current_index, aRow.end(),  [first, last] (const int& val) {
 				return val >= first && val < last;
 			});
 
@@ -277,10 +277,220 @@ int MPI_1D_Partitioning(int argc, char** argv) {
 	return 0;
 }
 
+int MPI_2D_Partitioning(int argc, char** argv) {
+	double t_start, t_end, time, total_time = 0;
+	double min = DBL_MAX;
+	double max = -DBL_MAX;
+	int n;		// number of nnz in a certain chunk
+	int row_size, col_size;
+	vector<tuple<int, int, float>> COOvect;
+	vector<int> aRow;
+	vector<int> aCol;
+	vector<float> aVal;
+	float* v = nullptr;
+	float* out = nullptr;
+
+	int rank, size;
+	int recv_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Request requests[5];
+	MPI_Status status;
+	MPI_Comm workers_comm;
+	int color;
+
+	if (size < 2) {
+		if (rank == 0)
+			fprintf(stderr, "Run with at least 2 processes.\n");
+		MPI_Finalize();
+		return 1;
+	}
+
+	if (rank == 0) {
+		color = MPI_UNDEFINED;
+	}
+	else {
+		color = 1;
+	}
+
+	MPI_Comm_split(MPI_COMM_WORLD, color, rank, &workers_comm);
+
+	if (rank == 0) {
+		sequential_execution(argc, argv);
+	}
+
+	int dims[2] = {0, 0};
+	MPI_Dims_create(size - 1, 2, dims);	// Get number of row and col chunks
+	int periods[2] = {0, 0};
+	int reorder = 1;
+	MPI_Comm cart_comm;
+	MPI_Cart_create(workers_comm, 2, dims, periods, reorder, &cart_comm);
+	int coords[2];
+	int cart_rank;
+	if (rank != 0) {
+		MPI_Comm_rank(cart_comm, &cart_rank);
+		MPI_Cart_coords(cart_comm, cart_rank, 2, coords);	// Each worker process gets its coordinates
+	}
+
+	// ---- MASTER CODE ----
+	if (rank == 0) {
+		cout << "--- MPI EXECUTION ---" << endl;
+		// Get input from command line
+		if (argc < 2) {
+			fprintf(stderr, "Usage: %s [martix-market-filename]\n", argv[0]);
+			MPI_Abort(MPI_COMM_WORLD, 1);
+		}
+		string filename = argv[1];
+
+		// -------- Fetch Matrix using vector
+		fetch_COO(COOvect, filename, aRow, aCol, aVal);
+
+		v = new float[col];
+		out = new float[row];
+		initV(v);
+	}
+
+	// Provide each process the number of cols
+	MPI_Bcast(&col, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	if (rank != 0) {
+		// Initialize v for all workers
+		v = new float[col];
+	}
+
+	// Provide each process the randomly generated vector
+	MPI_Bcast(v, col, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+
+	// Setup
+	if (rank == 0) {
+		int spare_rows = row % dims[0];
+		int spare_cols = col % dims[1];
+		row_size = (row - spare_rows) / dims[0];		// Number of rows per chunk
+		col_size = (col - spare_cols) / dims[1];		// Number of cols per chunk
+		int first_row = 0;	// first row of the chunk
+		int last_row;		// last row of the chunk
+		int first_col = 0;	// first col of the chunk
+		int last_col;		// last col of the chunk
+		int tmp;
+		int worker_world_rank;
+		MPI_Group world_group, cart_group;
+		MPI_Comm_group(MPI_COMM_WORLD, &world_group);
+		MPI_Comm_group(cart_comm, &cart_group);
+
+		for (int i = 0; i < dims[0]; i++) {		// For: number of row chunks
+			if (i < spare_rows) {
+				last_row = first_row + row_size + 1;
+			} else {
+				last_row = first_row + row_size;
+			}
+			for (int j = 0; j < dims[1]; j++) {	// For: number of col chunks
+				vector<int> aRowB;
+				vector<int> aColB;
+				vector<float> aValB;
+				if (j < spare_cols) {
+					last_col = first_col + col_size + 1;
+				} else {
+					last_col = first_col + col_size;
+				}
+
+				for_each(COOvect.begin(), COOvect.end(), [first_row, first_col, last_row, last_col, &aRowB, &aColB, &aValB] (const tuple<int, int, float>& val) {
+					int r = get<0>(val);
+					int c = get<1>(val);
+					if (r >= first_row && r <= last_row && c >= first_col && c <= last_col) {
+						aRowB.push_back(r);
+						aColB.push_back(c);
+						aValB.push_back(get<2>(val));
+					}
+				});
+
+				n = aValB.size();
+
+				/*
+				n = count_if(COOvect.begin(), COOvect.end(),  [first_row, first_col, last_row, last_col, &aRowB, &aColB, &aValB] (const tuple<int, int, float>& val) {
+					if ((std::get<0>(val) >= first_row && std::get<0>(val) < last_row) && (std::get<1>(val) >= first_col && std::get<1>(val) < last_col)) {
+						aRowB.push_back(get<0>(val));
+						aColB.push_back(get<1>(val));
+						aValB.push_back(get<2>(val));
+						return true;
+					}
+					return false;
+				});		// Count number of nnz elements inside the chunk
+				*/
+
+				// Get rank of process at coordinate (i, j)
+				coords[0] = i; coords[1] = j;
+				MPI_Cart_rank(cart_comm, coords, &cart_rank);
+				MPI_Group_translate_ranks(cart_group, 1, &cart_rank, world_group, &recv_rank);
+
+				// Get and send number of rows and cols the i,j process has to work on
+				tmp = last_row - first_row;
+				MPI_Send(&tmp, 1, MPI_INT, recv_rank, 0, MPI_COMM_WORLD);
+				tmp = last_col - first_col;
+				MPI_Send(&tmp, 1, MPI_INT, recv_rank, 1, MPI_COMM_WORLD);
+
+				MPI_Send(&n, 1, MPI_INT, recv_rank, 2, MPI_COMM_WORLD);		// Send number of nnz elements inside the chunk
+				MPI_Send(aRowB.data(), n, MPI_INT, recv_rank, 3, MPI_COMM_WORLD);		// Send chunk aRow
+				MPI_Send(aColB.data(), n, MPI_INT, recv_rank, 4, MPI_COMM_WORLD);		// Send chunk aCol
+				MPI_Send(aValB.data(), n, MPI_FLOAT, recv_rank, 5, MPI_COMM_WORLD);		// Send chunk aVal
+
+
+				first_col = last_col;
+			}
+
+
+
+			first_row = last_row;
+		}
+
+
+	} else {
+		MPI_Recv(&row, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);		// Receive number of rows of the chunk
+		MPI_Recv(&col, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);		// Receive number of cols of the chunk
+		MPI_Recv(&nnz, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);		// Receive number of nnz inside the chunk
+		aRow.resize(nnz);
+		aCol.resize(nnz);
+		aVal.resize(nnz);
+
+		MPI_Recv(aRow.data(), nnz, MPI_INT, 0, 3, MPI_COMM_WORLD, &status);	// Receive chunk aRow
+		MPI_Recv(aCol.data(), nnz, MPI_INT, 0, 4, MPI_COMM_WORLD, &status);	// Receive chunk aCol
+		MPI_Recv(aVal.data(), nnz, MPI_FLOAT, 0, 5, MPI_COMM_WORLD, &status);	// Receive chunk aVal
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	if (rank == 0) {
+		printf("Setup Finished\n");
+	}
+
+	// DEBUG PRINT
+	if (rank != 0) {
+		for (int i = 0; i < dims[0]; i++) {
+			for (int j = 0; j < dims[1]; j++) {
+				if (i == coords[0] && j == coords[1]) {
+					printf("\t--- World Rank: %d with coordinates (%d, %d)", rank, coords[0], coords[1]);
+					cout << "--- aRow: ---" << endl;
+					printVector(aRow);
+					cout << "--- aCol: ---" << endl;
+					printVector(aCol);
+					cout << "--- aVal: ---" << endl;
+					printVector(aVal);
+					fflush(stdout);
+				}
+				MPI_Barrier(cart_comm);
+			}
+		}
+	}
+
+	return 0;
+}
+
 int main(int argc, char** argv) {
 	MPI_Init(&argc, &argv);
 
-	MPI_1D_Partitioning(argc, argv);
+	//MPI_1D_Partitioning(argc, argv);
+
+	MPI_2D_Partitioning(argc, argv);
 
 	MPI_Finalize();
 
