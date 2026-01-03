70770abee9ea8e70cb509d7afba38bbdf873fd1d
FrancescoBrunelli <brunellifrancesco21@gmail.com>
2026-01-03 18:52:13 +0100

    Minor changes

--- D2/src/main.cpp
+++ D2/src/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(int argc, char** argv) {
 	MPI_Init(&argc, &argv);
 
-	sequential_execution(argc, argv);
+	//sequential_execution(argc, argv);
 
 	MPI_1D_Partitioning(argc, argv);
 
