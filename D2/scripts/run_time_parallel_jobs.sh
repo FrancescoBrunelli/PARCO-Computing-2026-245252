2f8d11a736eb853fe0d0e42383b3841baba40870
FrancescoBrunelli <brunellifrancesco21@gmail.com>
2026-01-04 18:57:55 +0100

    Fixed compilation

--- D2/scripts/run_time_parallel_jobs.sh
+++ D2/scripts/run_time_parallel_jobs.sh
@@ -4,6 +4,10 @@ MAX_PROCS=128
 INPUT="$1"    # Input from terminal
 #MODE="$2"    # Input from terminal
 
+# Load modules
+module load gcc91
+module load mpich-3.2.1--gcc-9.1.0
+
 mpicxx -std=c++11 -O3 src/main.cpp src/matrix.cpp src/mpiSpMV.cpp -o src/main.out # C++ code
 
 # for NPROCS in 2 4 8 16 32 64 128
