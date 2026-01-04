822f9b9042696134bf339b353aadede38efe5bae
FrancescoBrunelli <brunellifrancesco21@gmail.com>
2026-01-04 18:25:38 +0100

    Improved script

--- D2/scripts/run_time_parallel_jobs.sh
+++ D2/scripts/run_time_parallel_jobs.sh
@@ -9,6 +9,9 @@ mpicxx -std=c++11 -O3 src/main.cpp src/matrix.cpp src/mpiSpMV.cpp -o src/main.ou
 # for NPROCS in 2 4 8 16 32 64 128
 for NPROCS in 2 4
 do
-  echo "Submitting job for NPROCS=$NPROCS"
-  qsub -v INPUT="$INPUT",NPROCS="$NPROCS",MODE="$MODE" scripts/run_time.pbs
+  for MODE in 0 1 2
+  do
+    echo "Submitting job for NPROCS=$NPROCS MODE=$MODE"
+    qsub -v INPUT="$INPUT",NPROCS="$NPROCS",MODE="$MODE" scripts/run_time.pbs
+  done
 done
