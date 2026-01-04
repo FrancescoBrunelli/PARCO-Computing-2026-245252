5b82a3e57dd81fc54eb6847bcda2774e5106ddef
FrancescoBrunelli <brunellifrancesco21@gmail.com>
2026-01-04 16:53:02 +0100

    Added function switch

--- D2/src/main.cpp
+++ D2/src/main.cpp
@@ -9,13 +9,27 @@ int main(int argc, char** argv) {
 
 	//sequential_execution(argc, argv);
 
-	MPI_1D_Partitioning(argc, argv);
+	if (argc < 3) {
+		fprintf(stderr, "Usage: %s [martix-market-filename] [mode: 0 for 1D-partitioning, 1 for 1D-cyclic partitioning, or 2 for 2D partitioning]\n", argv[0]);
+		MPI_Abort(MPI_COMM_WORLD, 1);
+	}
 
-	MPI_1D_CyclingPartitioning(argc, argv);
-
-	MPI_2D_Partitioning(argc, argv);
+	int mode = stoi(argv[2]);
+	switch (mode) {
+		case 0: {
+			MPI_1D_Partitioning(argc, argv);
+			break;
+		}
+		case 1: {
+			MPI_1D_CyclingPartitioning(argc, argv);
+			break;
+		}
+		case 2: {
+			MPI_2D_Partitioning(argc, argv);
+			break;
+		}
+	}
 
 	MPI_Finalize();
-
 	return 0;
 }
