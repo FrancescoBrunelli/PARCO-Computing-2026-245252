2def80b2e6307a3bf1f434360335a8a945b97213
FrancescoBrunelli <brunellifrancesco21@gmail.com>
2026-01-05 10:48:46 +0100

    Final version

--- D2/scripts/run_time_parallel_jobs.sh
+++ D2/scripts/run_time_parallel_jobs.sh
@@ -10,8 +10,7 @@ module load mpich-3.2.1--gcc-9.1.0
 
 mpicxx -std=c++11 -O3 src/main.cpp src/matrix.cpp src/mpiSpMV.cpp -o src/main.out # C++ code
 
-# for NPROCS in 2 4 8 16 32 64 128
-for NPROCS in 2 4
+for NPROCS in 2 4 8 16 32 64 128
 do
   for MODE in 0 1 2
   do
